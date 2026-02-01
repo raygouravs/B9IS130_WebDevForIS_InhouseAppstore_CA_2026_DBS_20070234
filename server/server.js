c4ad982f530cd82ee1d4185a11648c10032c24b0
gouravray <gourav.sankar93@gmail.com>
2026-02-01 02:06:54 +0000

    server directory setup with a test route in server.js file

---
+++ server/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Test Route
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is humming along on port ${PORT}`);
+});
