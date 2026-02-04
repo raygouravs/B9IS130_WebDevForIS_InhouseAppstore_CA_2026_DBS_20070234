2987e111b3f7e28554add6ce462c68a7fc2cb8af
gouravray <gourav.sankar93@gmail.com>
2026-02-04 21:49:54 +0000

    db connection code set up

--- server/app.js
+++ server/app.js
@@ -1,5 +1,10 @@
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./config/db";
+
+dotenv.config();
+connectDB();
 
 const app = express();
 
